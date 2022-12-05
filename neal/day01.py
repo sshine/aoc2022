f4b2c539a45bcb517a633534694664bfd9f81ae1
n <neal.norman@icloud.com>
2022-12-05 15:57:09 +0100

    day 1 done

---
+++ neal/day01.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from aoclib import read_input
+import sys
+
+def part1(input):
+    groups = input.split("\n\n")
+    list_of_all_totals = map(total_calories, groups)
+
+    return max(list_of_all_totals)
+
+
+def total_calories(group):
+    # 1. split string into list of items
+    items = group.split("\n")
+    # 2. convert list of strings into list of numbers
+    calories = map(int, items)
+    # 3. summarise the calories
+    total = sum(calories)
+    return total
+
+def part2(input):
+    groups = input.split("\n\n")
+    list_of_all_totals = map(total_calories, groups)
+    #can not get map to turn into a list so....
+    newlist = []
+    for total in list_of_all_totals:
+        newlist.append(total)
+    return sum(sorted(newlist)[-3:])
+
+
+
+
+    return "nope"
+
+if __name__ == "__main__":
+    day_input = read_input(f"./input/{sys.argv[0][:-2]}txt")
+    print(f"{sys.argv[0][:-3]}, part 1: {part1(day_input)}")
+    print(f"{sys.argv[0][:-3]} part 2: {part2(day_input)}")
