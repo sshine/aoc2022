f702b3efe0a2a0c349c45fa60f903e250a3841f0
Simon Shine <simon@simonshine.dk>
2022-12-07 05:51:02 +0100

    Use python3-autopep8 for auto-formatting code
    https://github.com/hhatto/autopep8
    
    In VSCode, enable "Format on Save" in preferences.
    
    You will automatically get consistently formatted code.
    
    This works great for most syntax, including Python.
    
    PEP 8 is a very popular Python style guide:
    
    https://peps.python.org/pep-0008/

--- neal/day01.py
+++ neal/day01.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+
 from aoclib import read_input
 import sys
+
 
 def part1(input):
     groups = input.split("\n\n")
     list_of_all_totals = map(total_calories, groups)
     return max(list_of_all_totals)
+
 
 def total_calories(group):
     # 1. split string into list of items
@@ -16,15 +19,17 @@ def total_calories(group):
     total = sum(calories)
     return total
 
+
 def part2(input):
     groups = input.split("\n\n")
     list_of_all_totals = map(total_calories, groups)
-    #can not get map to turn into a list so....
+    # can not get map to turn into a list so....
     newlist = []
     for total in list_of_all_totals:
         newlist.append(total)
     #sort and cut and kill and eat
     return sum(sorted(newlist)[-3:])
+
 
 if __name__ == "__main__":
     day_input = read_input(f"./input/{sys.argv[0][:-2]}txt")
