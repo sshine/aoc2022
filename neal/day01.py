2cdd8ad9d1244fe41d446b7befb89294b8b473b5
Simon Shine <simon@simonshine.dk>
2022-12-07 06:03:26 +0100

    Drop intermediate list conversion
    `sorted()` does not need a list input, it works on iterables.
    
    If you needed to, you can use `list(map(f, ...))`, but it isn't
    necessary here and only consumes more memory (constructing the list).

--- neal/day01.py
+++ neal/day01.py
@@ -22,13 +22,7 @@ def total_calories(group):
 
 def part2(input):
     groups = input.split("\n\n")
-    list_of_all_totals = map(total_calories, groups)
-    # can not get map to turn into a list so....
-    newlist = []
-    for total in list_of_all_totals:
-        newlist.append(total)
-    #sort and cut and kill and eat
-    return sum(sorted(newlist)[-3:])
+    return sum(sorted(map(total_calories, groups))[-3:])
 
 
 if __name__ == "__main__":
